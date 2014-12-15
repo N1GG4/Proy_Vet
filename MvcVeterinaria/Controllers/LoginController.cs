f378876f43c74ff267fe9e6c3ae0b9448e719dc4
Roberto <polar.brv14@gmail.com>
2014-12-15 14:05:50 -0500

    creando controller para el login

---
+++ MvcVeterinaria/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MvcVeterinaria.Models;
+namespace MvcVeterinaria.Controllers
+{
+    public class LoginController : Controller
+    {
+        VeterinariaDB db = new VeterinariaDB();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
