f9b7fde585fccd380f625e0dea23796aefafdf15
Roberto <polar.brv14@gmail.com>
2014-12-15 13:24:38 -0500

    creando tabla cliente

---
+++ MvcVeterinaria/Models/tb_cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+namespace MvcVeterinaria.Models
+{
+    public class tb_cliente
+    {
+        [Key]
+        public string cod_cli { get; set; }
+        public string pass_cli { get; set; }
+        public string nom_cli { get; set; }
+        public string ape_cli { get; set; }
+        public string correo_cli { get; set; }
+    }
+}
